1a3dac828e9be0700e8779d731ae662e625add34
Bill VanDuzee <bill.vanduzee@availity.com>
2022-01-06 08:03:15 -0500

    test change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	Some change to see if it automatically deploys
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
